7726e2e152e4d364f20defbe29d864668ebd6c9a
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2024-01-03 19:19:13 -0500

    working on displaying text for components

--- murachsC#/questions/ch2/InvoiceTotal/InvoiceTotal/InvoiceTotal.Designer.cs
+++ murachsC#/questions/ch2/InvoiceTotal/InvoiceTotal/InvoiceTotal.Designer.cs
@@ -28,154 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InvoiceTotal));
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtSubtotal = new System.Windows.Forms.TextBox();
+            this.txtDiscountPercent = new System.Windows.Forms.TextBox();
+            this.txtDiscountAmount = new System.Windows.Forms.TextBox();
+            this.txtBoxTotal = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.label1, "label1");
             this.label1.ForeColor = System.Drawing.Color.LightPink;
-            this.label1.Location = new System.Drawing.Point(14, 76);
-            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 21);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Subtotal:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(134, 28);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(219, 21);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Shawn\'s Invoice Total Form";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 126);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(144, 21);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Discount Percent:";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(14, 177);
+            resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(148, 21);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Discount Amount:";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(14, 228);
+            resources.ApplyResources(this.label5, "label5");
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(52, 21);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Total:";
             // 
-            // button1
+            // btnCalculate
             // 
-            this.button1.BackColor = System.Drawing.Color.Gray;
-            this.button1.Location = new System.Drawing.Point(138, 294);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 32);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Calculate";
-            this.button1.UseVisualStyleBackColor = false;
+            resources.ApplyResources(this.btnCalculate, "btnCalculate");
+            this.btnCalculate.BackColor = System.Drawing.Color.Gray;
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // btnExit
             // 
-            this.button2.BackColor = System.Drawing.Color.Gray;
-            this.button2.Location = new System.Drawing.Point(259, 294);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 32);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Exit";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            resources.ApplyResources(this.btnExit, "btnExit");
+            this.btnExit.BackColor = System.Drawing.Color.Gray;
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox1
+            // txtSubtotal
             // 
-            this.textBox1.BackColor = System.Drawing.Color.DimGray;
-            this.textBox1.ForeColor = System.Drawing.Color.LightPink;
-            this.textBox1.Location = new System.Drawing.Point(215, 68);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(138, 29);
-            this.textBox1.TabIndex = 7;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            resources.ApplyResources(this.txtSubtotal, "txtSubtotal");
+            this.txtSubtotal.BackColor = System.Drawing.Color.DimGray;
+            this.txtSubtotal.ForeColor = System.Drawing.Color.LightPink;
+            this.txtSubtotal.Name = "txtSubtotal";
+            this.txtSubtotal.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // txtDiscountPercent
             // 
-            this.textBox2.BackColor = System.Drawing.Color.DimGray;
-            this.textBox2.ForeColor = System.Drawing.Color.LightPink;
-            this.textBox2.Location = new System.Drawing.Point(215, 123);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(138, 29);
-            this.textBox2.TabIndex = 8;
+            resources.ApplyResources(this.txtDiscountPercent, "txtDiscountPercent");
+            this.txtDiscountPercent.BackColor = System.Drawing.Color.DimGray;
+            this.txtDiscountPercent.ForeColor = System.Drawing.Color.LightPink;
+            this.txtDiscountPercent.Name = "txtDiscountPercent";
+            this.txtDiscountPercent.ReadOnly = true;
+            this.txtDiscountPercent.TabStop = false;
+            this.txtDiscountPercent.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // textBox3
+            // txtDiscountAmount
             // 
-            this.textBox3.BackColor = System.Drawing.Color.DimGray;
-            this.textBox3.ForeColor = System.Drawing.Color.LightPink;
-            this.textBox3.Location = new System.Drawing.Point(215, 177);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(138, 29);
-            this.textBox3.TabIndex = 9;
+            resources.ApplyResources(this.txtDiscountAmount, "txtDiscountAmount");
+            this.txtDiscountAmount.BackColor = System.Drawing.Color.DimGray;
+            this.txtDiscountAmount.ForeColor = System.Drawing.Color.LightPink;
+            this.txtDiscountAmount.Name = "txtDiscountAmount";
+            this.txtDiscountAmount.ReadOnly = true;
+            this.txtDiscountAmount.TabStop = false;
+            this.txtDiscountAmount.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
-            // textBox4
+            // txtBoxTotal
             // 
-            this.textBox4.BackColor = System.Drawing.Color.DimGray;
-            this.textBox4.ForeColor = System.Drawing.Color.LightPink;
-            this.textBox4.Location = new System.Drawing.Point(215, 228);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(138, 29);
-            this.textBox4.TabIndex = 10;
+            resources.ApplyResources(this.txtBoxTotal, "txtBoxTotal");
+            this.txtBoxTotal.BackColor = System.Drawing.Color.DimGray;
+            this.txtBoxTotal.ForeColor = System.Drawing.Color.LightPink;
+            this.txtBoxTotal.Name = "txtBoxTotal";
+            this.txtBoxTotal.ReadOnly = true;
+            this.txtBoxTotal.TabStop = false;
+            this.txtBoxTotal.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // InvoiceTotal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AcceptButton = this.btnCalculate;
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(54)))), ((int)(((byte)(66)))));
-            this.ClientSize = new System.Drawing.Size(466, 405);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.CancelButton = this.btnExit;
+            this.Controls.Add(this.txtBoxTotal);
+            this.Controls.Add(this.txtDiscountAmount);
+            this.Controls.Add(this.txtDiscountPercent);
+            this.Controls.Add(this.txtSubtotal);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.LightPink;
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.KeyPreview = true;
             this.Name = "InvoiceTotal";
-            this.Text = "Subtotal:";
             this.Load += new System.EventHandler(this.InvoiceTotal_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -185,16 +150,15 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtSubtotal;
+        private System.Windows.Forms.TextBox txtDiscountPercent;
+        private System.Windows.Forms.TextBox txtDiscountAmount;
+        private System.Windows.Forms.TextBox txtBoxTotal;
     }
 }
 
