da3b7692a7c0921132b1d3f37f778fd8a5c86a17
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2024-01-04 03:46:03 -0500

    Completed form ch 2

--- murachsC#/questions/ch2/InvoiceTotal/InvoiceTotal/InvoiceTotal.Designer.cs
+++ murachsC#/questions/ch2/InvoiceTotal/InvoiceTotal/InvoiceTotal.Designer.cs
@@ -30,15 +30,15 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InvoiceTotal));
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.btnCalculate = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.txtSubtotal = new System.Windows.Forms.TextBox();
             this.txtDiscountPercent = new System.Windows.Forms.TextBox();
             this.txtDiscountAmount = new System.Windows.Forms.TextBox();
-            this.txtBoxTotal = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -48,53 +48,47 @@
             this.label1.Name = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label4_Click);
+            // 
             // label3
             // 
             resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
-            // 
-            // label4
-            // 
-            resources.ApplyResources(this.label4, "label4");
-            this.label4.Name = "label4";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
-            // 
-            // label5
-            // 
-            resources.ApplyResources(this.label5, "label5");
-            this.label5.Name = "label5";
             // 
             // btnCalculate
             // 
-            resources.ApplyResources(this.btnCalculate, "btnCalculate");
             this.btnCalculate.BackColor = System.Drawing.Color.Gray;
+            resources.ApplyResources(this.btnCalculate, "btnCalculate");
             this.btnCalculate.Name = "btnCalculate";
             this.btnCalculate.UseVisualStyleBackColor = false;
             this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnExit
             // 
-            resources.ApplyResources(this.btnExit, "btnExit");
             this.btnExit.BackColor = System.Drawing.Color.Gray;
             this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.btnExit, "btnExit");
             this.btnExit.Name = "btnExit";
             this.btnExit.UseVisualStyleBackColor = false;
             this.btnExit.Click += new System.EventHandler(this.button2_Click);
             // 
             // txtSubtotal
             // 
-            resources.ApplyResources(this.txtSubtotal, "txtSubtotal");
             this.txtSubtotal.BackColor = System.Drawing.Color.DimGray;
             this.txtSubtotal.ForeColor = System.Drawing.Color.LightPink;
+            resources.ApplyResources(this.txtSubtotal, "txtSubtotal");
             this.txtSubtotal.Name = "txtSubtotal";
             this.txtSubtotal.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // txtDiscountPercent
             // 
-            resources.ApplyResources(this.txtDiscountPercent, "txtDiscountPercent");
             this.txtDiscountPercent.BackColor = System.Drawing.Color.DimGray;
             this.txtDiscountPercent.ForeColor = System.Drawing.Color.LightPink;
+            resources.ApplyResources(this.txtDiscountPercent, "txtDiscountPercent");
             this.txtDiscountPercent.Name = "txtDiscountPercent";
             this.txtDiscountPercent.ReadOnly = true;
             this.txtDiscountPercent.TabStop = false;
@@ -102,23 +96,29 @@
             // 
             // txtDiscountAmount
             // 
-            resources.ApplyResources(this.txtDiscountAmount, "txtDiscountAmount");
             this.txtDiscountAmount.BackColor = System.Drawing.Color.DimGray;
             this.txtDiscountAmount.ForeColor = System.Drawing.Color.LightPink;
+            resources.ApplyResources(this.txtDiscountAmount, "txtDiscountAmount");
             this.txtDiscountAmount.Name = "txtDiscountAmount";
             this.txtDiscountAmount.ReadOnly = true;
             this.txtDiscountAmount.TabStop = false;
             this.txtDiscountAmount.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
-            // txtBoxTotal
+            // txtTotal
             // 
-            resources.ApplyResources(this.txtBoxTotal, "txtBoxTotal");
-            this.txtBoxTotal.BackColor = System.Drawing.Color.DimGray;
-            this.txtBoxTotal.ForeColor = System.Drawing.Color.LightPink;
-            this.txtBoxTotal.Name = "txtBoxTotal";
-            this.txtBoxTotal.ReadOnly = true;
-            this.txtBoxTotal.TabStop = false;
-            this.txtBoxTotal.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.txtTotal.BackColor = System.Drawing.Color.DimGray;
+            this.txtTotal.ForeColor = System.Drawing.Color.LightPink;
+            resources.ApplyResources(this.txtTotal, "txtTotal");
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.TabStop = false;
+            this.txtTotal.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            this.label4.Click += new System.EventHandler(this.label2_Click);
             // 
             // InvoiceTotal
             // 
@@ -128,19 +128,20 @@
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(54)))), ((int)(((byte)(66)))));
             this.CancelButton = this.btnExit;
-            this.Controls.Add(this.txtBoxTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTotal);
             this.Controls.Add(this.txtDiscountAmount);
             this.Controls.Add(this.txtDiscountPercent);
             this.Controls.Add(this.txtSubtotal);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnCalculate);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.Color.LightPink;
             this.KeyPreview = true;
             this.Name = "InvoiceTotal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.Load += new System.EventHandler(this.InvoiceTotal_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -150,15 +151,15 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button btnCalculate;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.TextBox txtSubtotal;
         private System.Windows.Forms.TextBox txtDiscountPercent;
         private System.Windows.Forms.TextBox txtDiscountAmount;
-        private System.Windows.Forms.TextBox txtBoxTotal;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- murachsC#/questions/ch2/InvoiceTotal/InvoiceTotal/InvoiceTotal.cs
+++ murachsC#/questions/ch2/InvoiceTotal/InvoiceTotal/InvoiceTotal.cs
@@ -49,5 +49,10 @@ namespace InvoiceTotal
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
