40b1b482c35c5f65aa11b69d8d97d49b199c3a03
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2024-01-07 17:43:39 -0500

    working on ch 5

--- murachsC#/questions/ch5/InvoiceTotal/InvoiceTotal/frmInvoiceTotal.cs
+++ murachsC#/questions/ch5/InvoiceTotal/InvoiceTotal/frmInvoiceTotal.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace InvoiceTotal
@@ -16,43 +23,25 @@ namespace InvoiceTotal
             decimal subtotal = Convert.ToDecimal(txtSubtotal.Text);
             decimal discountPercent = .0m;
 
-            if (customerType == "R" || customerType == "r")
+            if (customerType == "R")
             {
                 if (subtotal < 100)
-                {
                     discountPercent = .0m;
-                }
                 else if (subtotal >= 100 && subtotal < 250)
-                {
                     discountPercent = .1m;
-                }
-                else if (subtotal >= 250 && subtotal < 500)
-                {
+                else if (subtotal >= 250)
                     discountPercent = .25m;
-                }
-                else
-                {
-                    discountPercent = .30m;
-                }
             }
-            else if (customerType == "C" || customerType == "c")
+            else if (customerType == "C")
             {
-                discountPercent = .2m;
-            }
-            else if (customerType == "T" || customerType == "t")
-            {
-                if (subtotal < 500)
-                {
-                    discountPercent = .4m;
-                }
+                if (subtotal < 250)
+                    discountPercent = .2m;
                 else
-                {
-                    discountPercent = .5m;
-                }
+                    discountPercent = .3m;
             }
             else
             {
-                discountPercent = .1m;
+                discountPercent = .4m;
             }
 
             decimal discountAmount = subtotal * discountPercent;
